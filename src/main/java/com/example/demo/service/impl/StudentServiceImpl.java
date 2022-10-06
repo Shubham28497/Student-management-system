3edd5fd821a1ad3b8cd07ab6588323233dfb8f61
Shubham28497 <74322538+Shubham28497@users.noreply.github.com>
2022-10-06 12:17:35 +0530

    student service impl updated

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,5 +1,30 @@
 package com.example.demo.service.impl;
 
-public class StudentServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+	private StudentRepository studentRepository;
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	
+	
+	
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return studentRepository.findAll();
+	}
 
 }
